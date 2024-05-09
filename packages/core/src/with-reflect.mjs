b476402e45d55229fd9446b6d785f921c62da259
Oleksii Olefirenko <olefirenk@gmail.com>
2024-05-09 15:55:30 -0400

    fix: with-reflect

--- packages/core/src/with-reflect.mjs
+++ packages/core/src/with-reflect.mjs
@@ -1,18 +1,14 @@
-/** @type {import('next').NextConfig} */
-export const withReflect = (nextConfig) => ({
-  webpack: (config) => {
-    const entry = async () => {
-      const _entry = await config.entry();
+export const withReflect = (config) => {
+  const entry = async () => {
+    const _entry = await config.entry();
 
-      const mainApp = _entry['main-app'] ?? [];
+    const mainApp = _entry['main-app'] ?? [];
 
-      return {
-        ..._entry,
-        'main-app': ['reflect-metadata/lite', ...mainApp],
-      };
+    return {
+      ..._entry,
+      'main-app': ['reflect-metadata/lite', ...mainApp],
     };
+  };
 
-    return { ...config, entry };
-  },
-  ...nextConfig,
-});
+  return { ...config, entry };
+};
--- packages/next-example/next.config.mjs
+++ packages/next-example/next.config.mjs
@@ -1,6 +1,8 @@
 import { withReflect } from '@next-inversify/core/src/with-reflect.mjs';
 
 /** @type {import('next').NextConfig} */
-const nextConfig = withReflect({});
+const nextConfig = withReflect({
+  webpack: (config) => withReflect(config),
+});
 
 export default nextConfig;
