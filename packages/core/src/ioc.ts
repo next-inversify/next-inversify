846d1d6b27898529ae0a820ff599a5d4037aee58
Oleksii Olefirenko <olefirenk@gmail.com>
2024-05-22 00:57:23 -0400

    feat: getContainer forward params

--- packages/core/src/ioc.ts
+++ packages/core/src/ioc.ts
@@ -5,19 +5,21 @@ import { isServer } from './utils/is-server';
 
 let container: Container;
 
-export const getContainer = (createContainer: () => Container) => () => {
-  if (isServer) {
-    const container = createContainer();
+export const getContainer =
+  <P extends any[]>(createContainer: (...params: P) => Container) =>
+  (...params: P) => {
+    if (isServer) {
+      const container = createContainer(...params);
+
+      return container;
+    }
+
+    if (!container) {
+      container = createContainer(...params);
+    }
 
     return container;
-  }
-
-  if (!container) {
-    container = createContainer();
-  }
-
-  return container;
-};
+  };
 
 let IOCBag: number[];
 
