b6cb44df0b6f840535dc5edaae25e04e4fe43924
Oleksii Olefirenko <olefirenk@gmail.com>
2024-08-01 19:58:54 -0400

    fix: hydration

--- packages/query/src/use-hydrate-cache.hook.ts
+++ packages/query/src/use-hydrate-cache.hook.ts
@@ -17,15 +17,16 @@ export const useHydrateCache = (params: UseHydrateCacheParams) => {
   useEffect(() => {
     const hydrationData: Record<string, QueryState> = {};
 
-    for (const [key, data] of Object.entries(params.data || {})) {
-      const query = queryCache.get(key);
+    for (const [key, newState] of Object.entries(params.data || {})) {
+      const cachedState = queryCache.get(key);
       /**
         hydrate in case:
-          - query doesn't have a staleAt which means it wasn't loaded on the server by some reason (failed query, etc)
-          - query has a fresh staleAt
+          - new query doesn't have a staleAt which means it wasn't loaded on the server by some reason (failed query, etc) - cleans data
+          - old query doesn't have a staleAt which means (new query was loaded on the server) - inserts data
+          - new query has a staleAt which is fresher than the one in the cache - updates data
       **/
-      if (!data.staleAt || (query.staleAt && data.staleAt > query.staleAt)) {
-        hydrationData[key] = data;
+      if (!newState.staleAt || !cachedState.staleAt || newState.staleAt > cachedState.staleAt) {
+        hydrationData[key] = newState;
       }
     }
 
